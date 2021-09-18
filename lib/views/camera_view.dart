57caf6cf23bb79117f104cdca22edc9bbe148932
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-09-18 17:14:24 -0400

    improved camera_view UI

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:htn/constants.dart';
 
 import '../main.dart' as main;
 
@@ -51,19 +52,87 @@ class _CameraViewState extends State<CameraView> {
     if (!controller!.value.isInitialized) {
       return Container();
     }
-    return MaterialApp(
-      home: Scaffold(
-        body: AspectRatio(
-          aspectRatio: controller!.value.aspectRatio > 1
-              ? 1 / controller!.value.aspectRatio
-              : controller!.value.aspectRatio,
-          child: CameraPreview(controller!),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            pickImage(ImageSource.gallery);
-          },
-          child: Icon(Icons.photo_library),
+    return Scaffold(
+      body: AspectRatio(
+        aspectRatio: controller!.value.aspectRatio > 1
+            ? 1 / controller!.value.aspectRatio
+            : controller!.value.aspectRatio,
+        child: CameraPreview(controller!),
+      ),
+      bottomSheet: Container(
+        height: MediaQuery.of(context).size.height * 0.2,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0)),
+            color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary:
+                          Colors.white, // <-- Button color <-- Splash color
+                      elevation: 2,
+                    ),
+                    child: Icon(Icons.arrow_back_rounded, color: Colors.black),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      side:
+                          BorderSide(width: 4, color: Constants.PrimaryYellow),
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary:
+                          Colors.white, // <-- Button color <-- Splash color
+                      elevation: 5,
+                    ),
+                    child: Positioned(
+                      child: Image(
+                        image: AssetImage('Assets/goose.png'),
+                        height: 50,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      pickImage(ImageSource.gallery);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary:
+                          Colors.white, // <-- Button color <-- Splash color
+                      elevation: 2,
+                    ),
+                    child: Icon(Icons.photo_library, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Find Goose",
+              style: TextStyle(fontSize: 20),
+            )
+          ],
         ),
       ),
     );
