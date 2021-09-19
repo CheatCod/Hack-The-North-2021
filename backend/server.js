dbdf64a8120bdadfd4a78483b16cda8f6c174f08
Ynng <44907675+Ynng@users.noreply.github.com>
2021-09-19 02:19:38 +0000

    changed port and stuff

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const tf = require("@tensorflow/tfjs-node");
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
